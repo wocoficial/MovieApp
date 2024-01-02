9e464ac31e49fa40f82caff4f8add825fb02e330
Wemerson Costa <wemerson.costa@woc.dev.br>
2024-01-02 13:33:38 -0300

    Adicionando dependência do hilt e do timber no projeto.

---
+++ app/src/main/java/br/dev/woc/movieapp/MovieApp.kt
@@ -0,0 +1,13 @@
+package br.dev.woc.movieapp
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
+
+@HiltAndroidApp
+class MovieApp : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        if (BuildConfig.DEBUG) Timber.plant(Timber.DebugTree())
+    }
+}
