7805ab990c0782dda46548fd432ac42fdbeaf6a8
Wemerson Costa <wemerson.costa@woc.dev.br>
2024-01-02 17:48:42 -0300

    Implementando quantidade de favoritos dos filmes.

---
+++ app/src/main/java/br/dev/woc/movieapp/movie_popular_feature/presentation/components/MovieRate.kt
@@ -0,0 +1,47 @@
+package br.dev.woc.movieapp.movie_popular_feature.presentation.components
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Star
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun MovieRate(
+    rate: Double, modifier: Modifier
+) {
+    Row(
+        modifier = modifier,
+        horizontalArrangement = Arrangement.spacedBy(2.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Icon(
+            imageVector = Icons.Default.Star,
+            contentDescription = null,
+            tint = Color.Yellow,
+            modifier = Modifier.size(12.dp)
+        )
+        Text(
+            text = rate.toString(),
+            style = MaterialTheme.typography.body1,
+            color = Color.White,
+            fontSize = 10.sp
+        )
+    }
+}
+
+@Preview
+@Composable
+fun MovieRatePreview() {
+    MovieRate(rate = 7.1, modifier = Modifier)
+}
