dfa673a60b247903ea8911431821b991a129d4f8
Wemerson Costa <wemerson.costa@woc.dev.br>
2024-01-02 12:10:00 -0300

    Adicionando interceptador de requisições no projeto.

---
+++ app/src/main/java/br/dev/woc/movieapp/core/data/remote/ParamsInterceptor.kt
@@ -0,0 +1,22 @@
+package br.dev.woc.movieapp.core.data.remote
+
+import br.dev.woc.movieapp.BuildConfig
+import br.dev.woc.movieapp.core.util.Constants
+import okhttp3.Interceptor
+import okhttp3.Response
+
+class ParamsInterceptor : Interceptor {
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val request = chain.request()
+        val url = request.url.newBuilder()
+            .addQueryParameter(Constants.LANGUAGE_PARAM, Constants.LANGUAGE_VALUE)
+            .addQueryParameter(Constants.API_KEY_PARAM, BuildConfig.API_KEY)
+            .build()
+
+        val newRquest = request.newBuilder()
+            .url(url)
+            .build()
+
+        return chain.proceed(newRquest)
+    }
+}
