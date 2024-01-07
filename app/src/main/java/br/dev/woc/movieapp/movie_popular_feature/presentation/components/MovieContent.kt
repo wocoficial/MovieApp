5b6cb208f2e13e4769fd7d1856601bb423d1f138
Wemerson Costa <wemerson.costa@woc.dev.br>
2024-01-07 06:33:48 -0300

    Tratando o estado da paginação dos filmes populares, com loading e error.

--- app/src/main/java/br/dev/woc/movieapp/movie_popular_feature/presentation/components/MovieContent.kt
+++ app/src/main/java/br/dev/woc/movieapp/movie_popular_feature/presentation/components/MovieContent.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.GridItemSpan
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -13,8 +14,11 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.paging.LoadState
 import androidx.paging.compose.LazyPagingItems
 import br.dev.woc.movieapp.core.domain.model.Movie
+import br.dev.woc.movieapp.core.presentation.components.common.ErrorScreen
+import br.dev.woc.movieapp.core.presentation.components.common.LoadingView
 
 @Composable
 fun MovieContent(
@@ -45,10 +49,59 @@ fun MovieContent(
                     }
                 }
             }
+            pagingMovies.apply {
+                when {
+                    loadState.refresh is LoadState.Loading -> {
+                        item(
+                            span = {
+                                GridItemSpan(maxLineSpan)
+                            }
+                        ) {
+                            LoadingView()
+                        }
+                    }
+
+                    loadState.append is LoadState.Loading -> {
+                        item(
+                            span = {
+                                GridItemSpan(maxLineSpan)
+                            }
+                        ) {
+                            LoadingView()
+                        }
+                    }
+
+                    loadState.refresh is LoadState.Error -> {
+                        item(
+                            span = {
+                                GridItemSpan(maxLineSpan)
+                            }
+                        ) {
+                            ErrorScreen(message = "Verifique a sua conexão com a internet",
+                                retry = {
+                                    retry()
+                                })
+                        }
+                    }
+
+                    loadState.append is LoadState.Error -> {
+                        item(
+                            span = {
+                                GridItemSpan(maxLineSpan)
+                            }
+                        ) {
+                            ErrorScreen(message = "Verifique a sua conexão com a internet",
+                                retry = {
+                                    retry()
+                                })
+                        }
+                    }
+                }
+            }
         }
     }
-
 }
+
 @Preview
 @Composable
 fun MovieContentPreview() {
